19290ee9daa22aab65fb41616d7f7e18eabcbd73
mohamed hussin <65326535+mhussin2008@users.noreply.github.com>
2022-11-19 17:43:30 +0200

    first edition

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class Constants
+{
+  static const DayName=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+  static const DayApprev=['St','Sn','Mn','Tu','Wd','Th','Fr'];
+
+}
